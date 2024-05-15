22cfdb2c776c143cedc0784e577232e33f359465
Abdullah Azhar Khan <abdullahazharkhan1@gmail.com>
2024-05-15 10:58:16 +0500

    check

--- main.py
+++ main.py
@@ -16,15 +16,15 @@ while True:
     if multiLandMarks:
         handPoints = []
         for handLms in multiLandMarks:
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+            # mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
             for idx, lm in enumerate(handLms.landmark):
                 h, w, c = img.shape
                 cx, cy = int(lm.x*w), int(lm.y*h)
                 handPoints.append((cx, cy))
 
-        for point in handPoints:
-            cv2.circle(img, point, 10, (255, 0, 255), cv2.FILLED)
+        # for point in handPoints:
+        #     cv2.circle(img, point, 10, (255, 0, 255), cv2.FILLED)
             
         upCount = 0
         flag = 0
@@ -45,6 +45,7 @@ while True:
             cv2.putText(img, str(upCount), (80, 150), cv2.FONT_HERSHEY_PLAIN, 12, (0, 255, 0), 12)
         else:
             cv2.putText(img, str(upCount), (80, 150), cv2.FONT_HERSHEY_PLAIN, 12, (255, 0, 0), 12)
-        
+
+        print(upCount)
     cv2.imshow("Finger Counter", img)
     cv2.waitKey(5)
